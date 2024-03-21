89e7e299d10090d45dc6eda388080c662b6f3112
LHHdiv <1067546112@qq.com>
2024-03-21 15:04:05 +0800

    feat: ✨  project init finish

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+// 解决main.ts中引用报错问题【找不到模块“./App.vue”或其相应的类型声明。ts(2307)】
+declare module '*.vue' {
+  // 引入vue模块中ts的方法
+  import type { DefineComponent } from 'vue'
+  // 定义vue组件以及类型注解
+  const component: DefineComponent<{}, {}>
+  export default component
+}
--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,4 +1,13 @@
 /* eslint-env node */
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier', 'stylelint-config-html/vue']
+  extends: ['stylelint-config-standard', 'stylelint-config-recommended-vue']
+  // stylelint规则可以在下面自己定义
+  // overrides: [
+  //   {
+  //     files: ['*.vue', '**/*.vue'],
+  //     rules: {
+  //       'unit-allowed-list': ['em', 'rem', 's', '%', 'px', 'vw', 'vh'] // 指定允许的单位列表。
+  //     }
+  //   }
+  // ]
 }
